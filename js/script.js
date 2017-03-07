ff8ed0db19b4e3e134853b538f333019f5973b9a
roe-el <rogar28@gmail.com>
2017-03-07 16:37:12 -0600

    Add JSON functions and html layout.

--- js/script.js
+++ js/script.js
@@ -1,33 +1,50 @@
 $(document).ready(function() {
-
+var j = 0;
             function streamData() {
-            	
+            	var array = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
                 console.log("Outside");
-                var array = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
                 for (var i = 0; i < array.length; i++) {
-                	var name = array[i];
+                	console.log('Outside 1st JSON:'+array[i]);
                     $.getJSON('https://wind-bow.gomix.me/twitch-api/streams/' + array[i] + '?callback=?', function(data) {
-                    		
+                    	    var array = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+                    		console.log('Inside 1st JSON:'+array[i]);
                             console.log(data);
                             if(data.stream!==null){
                             $('.results').append("<a href='"
-                            	+ data.url
-                            	+ "' target='_blank'><div class='row well'><div class='col-md-4'><img src='"
-                            	+data.logo 
-                                +"' alt=''></div><div class='col-md-4'>"
+                            	+ data._links.channel
+                            	+ "' target='_blank'><div class='row well success'><div class='col-md-3'><img class='img-thumbnail img-responsive' src='"
+                            	+data.stream.preview.medium 
+                                +"' alt=''></div><div class='col-md-3'>"
+                                + data.stream.channel.display_name
+                                +"</div><div class='col-md-3'>" 
+                                +data.stream.game
+                                +"</div><div class='col-md-3'>"
+                                +data.stream.viewers
+                                +"</div></a>").fadeIn();
+                            } else{
+                    	    var array = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+                    	    
+                    $.getJSON("https://wind-bow.gomix.me/twitch-api/channels/"+array[j]+"?callback=?", function(d){
+                    	    if(d.status!=404){var array = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+                            	console.log('Inside 2nd JSON:'+array[j]);
+                            	console.log(d);
+                            	$('.results').append("<a href='"
+                            	+ d.url
+                            	+ "' target='_blank'><div class='row well warning'><div class='col-md-3'><img class='img-thumbnail img-responsive' src='"
+                            	+d.logo
+                                +"' alt=''></div><div class='col-md-3'>"
                                 + data.display_name
-                                +"</div><div class='col-md-4'>" 
-                                +data.status
-                                +"</div></div></a>").fadeIn();
+                                +"</div><div class='col-md-3'>" 
+                                +d.game
+                                +"</div><div class='col-md-3'>Not Streaming</div></a>").fadeIn();
+                            } else{
+                            	$('.results').append("<div class='row well danger'><div class='col-md-3'><img class='img-thumbnail' src=''alt='None'></div><div class='col-md-3'>Does Not Exist</div><div class='col-md-3'>Does Not Exist</div><div class='col-md-3'>Does Not Exist</div>").fadeIn();
                             }
-                            
-                            	else{
-                            	$('.results').append("<div class='row well'>"
-                            		+data.message+
-                            	"</div>").fadeIn();
+                            	});
+                            	j++;
                             }
                             });
                     }
                 }
-                streamData();
+                streamData(); img-responsive
             });
