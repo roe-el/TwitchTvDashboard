8abad1007efd3c8ffae85c41fd09355d461f2b9b
roe-el <rogar28@gmail.com>
2017-04-04 17:39:23 -0500

    Have functions working and split up for easier reading. Also add $.each().

--- js/script.js
+++ js/script.js
@@ -1,19 +1,27 @@
 $(document).ready(function() {
 var j = 0;
             function streamData() {
-            	var array = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+            	var streamers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
                 console.log("Outside");
-                for (var i = 0; i < array.length; i++) {
-                	console.log('Outside 1st JSON:'+array[i]);
-                    $.getJSON('https://wind-bow.gomix.me/twitch-api/streams/' + array[i] + '?callback=?', function(data) {
-                    	    var array = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
-                    		console.log('Inside 1st JSON:'+array[i]);
+                $.each(streamers, function(i, val)  {
+                	console.log('Outside 1st JSON:'+val);
+                    $.getJSON('https://wind-bow.gomix.me/twitch-api/streams/' + val+ '?callback=?', function(data) {
+                    	    var streamers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+                    		console.log('Inside 1st JSON:'+val);
                             console.log(data);
                             if(data.stream!=null){
-                            $('.results').append("<a href='"
-                            	+ data.stream.channel.url
-                            	+ "' target='_blank'><div class='row well stream'><div class='col-md-3'><img class='img-thumbnail img-circle img-responsive pic' src='"
-                            	+data.stream.preview.medium 
+                                postResults(data);
+                            } 
+                            else{
+                    	    channelInfo(val);	
+                            }
+                            });
+                    });
+                    function postResults(data){
+                        $('.results').append("<a href='"
+                                + data.stream.channel.url
+                                + "' target='_blank'><div class='row well stream'><div class='col-md-3'><img class='img-thumbnail img-circle img-responsive pic' src='"
+                                +data.stream.preview.medium 
                                 +"' alt=''></div><div class='col-md-3'>"
                                 + data.stream.channel.display_name
                                 +"</div><div class='col-md-3'>" 
@@ -21,29 +29,27 @@ var j = 0;
                                 +"</div><div class='col-md-3'>"
                                 +data.stream.viewers
                                 +"</div></a>").fadeIn();
-                            } else{
-                    	    var array = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
-                    	    
-                    $.getJSON("https://wind-bow.gomix.me/twitch-api/channels/"+array[j]+"?callback=?", function(d){
-                    	    if(d.status!=404){var array = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
-                            	console.log('Inside 2nd JSON:'+array[j]);
-                            	console.log(d);
-                            	$('.results').append("<a href='"
-                            	+ d.url
-                            	+ "' target='_blank'><div class='row well stream'><div class='col-md-3'><img class='img-thumbnail img-circle img-responsive pic' src='"
-                            	+d.logo
+                    }
+                    function channelInfo(val){
+                        $.getJSON("https://wind-bow.gomix.me/twitch-api/channels/"+val+"?callback=?", function(d){
+                            {    if(d.status!=404){
+                                console.log('Inside 2nd JSON:'+val);
+                                console.log(d);
+                                $('.results').append("<a href='"
+                                + d.url
+                                + "' target='_blank'><div class='row well stream'><div class='col-md-3'><img class='img-thumbnail img-circle img-responsive pic' src='"
+                                +d.logo
                                 +"' alt=''></div><div class='col-md-3'>"
                                 +d.display_name
                                 +"</div><div class='col-md-3'>" 
                                 +d.game
-                                +"</div><div class='col-md-3'>Not Streaming</div></a>").fadeIn();
-                            } else{
-                            	$('.results').append("<div class='row well noStream'><div class='col-md-3'><img class='img-thumbnail' src=''alt='None'></div><div class='col-md-3'>Does Not Exist</div><div class='col-md-3'>Does Not Exist</div><div class='col-md-3'>Does Not Exist</div>").fadeIn();
+                                +"</div><div class='col-md-3'>Not Streaming</div></a>").fadeIn();}
+                                else{
+                                 $('.results').append("<div class='row well noStream'><div class='col-md-3'><img class='img-thumbnail' src=''alt='None'></div><div class='col-md-3'>Does Not Exist</div><div class='col-md-3'>Does Not Exist</div><div class='col-md-3'>Does Not Exist</div>").fadeIn();
+
+                                }
                             }
-                            	});
-                            	
-                            }j++;
-                            });
+                                });
                     }
                 }
                 streamData();
